743bfca5c524b0bba1f0d266fee222848f30d9c9
Vachagan Mirzoian <vchgnmirms@gmail.com>
2021-04-29 08:56:37 +0400

    1st edit

--- GithubUsage/Program.cs
+++ GithubUsage/Program.cs
@@ -6,7 +6,7 @@ namespace GithubUsage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Text"); //Commit All
         }
     }
 }
