912990939b64668f3b3c159fd60507866f28a74b
Vachagan Mirzoian <vchgnmirms@gmail.com>
2021-05-11 09:51:16 +0400

    5th edit recommit

--- GithubUsage/Program.cs
+++ GithubUsage/Program.cs
@@ -10,8 +10,7 @@ namespace GithubUsage
             Console.WriteLine("Text"); //Commit All and Push
             Console.WriteLine("Text"); //Commit All
             Console.WriteLine("Text"); //Commit All and Sync
-
-
+            Console.WriteLine("Text"); //Commit All and Sync
         }
     }
 }
